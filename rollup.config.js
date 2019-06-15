1441cad1d3db1e1af5229fa3d6b4fc7d4dcdde91
cnwhy <w.why@163.com>
2019-06-15 14:04:01 +0800

    out min file

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,22 @@
 import typescript from 'rollup-plugin-typescript';
-import babel from 'rollup-plugin-babel'
+import babel from 'rollup-plugin-babel';
+import { uglify } from 'rollup-plugin-uglify';
+import { terser } from 'rollup-plugin-terser';
 // import es3 from 'rollup-plugin-es3'
-import pkg from './package.json'
+import pkg from './package.json';
 
-let banner =`/*!
+let banner = `/*!
  * ${pkg.name}  v${pkg.version}
  * Homepage ${pkg.homepage}
  * License ${pkg.license}
  */
-`
+`;
+
+let minOpts = {
+	output: {
+		preamble: banner
+	}
+};
 
 export default [
 	{
@@ -17,7 +25,7 @@ export default [
 			typescript({
 				target: 'ES2015',
 				module: 'ES2015',
-				removeComments: true,
+				removeComments: true
 			})
 		],
 		output: [
@@ -26,7 +34,7 @@ export default [
 				// file: outDir + 'Base64.es.js',
 				format: 'es',
 				banner: banner
-			},
+			}
 			// {
 			// 	file: pkg.main,
 			// 	// file: outDir + 'Base64.umd.js',
@@ -40,13 +48,47 @@ export default [
 		plugins: [
 			babel({
 				babelrc: false,
-				presets: [['@babel/env', { targets: "ie 6" }]],
-			}),
+				presets: [['@babel/env', { targets: 'ie 6' }]]
+			})
 			// es3()
 		],
 		output: [
 			{
 				file: pkg.main,
+				name: 'Base64',
+				format: 'umd'
+			}
+		]
+	},
+	{
+		input: './src/Base64.ts',
+		plugins: [
+			typescript({
+				target: 'ES2015',
+				module: 'ES2015',
+				removeComments: true
+			}),
+			terser(minOpts)
+		],
+		output: [
+			{
+				file: pkg.module.replace(/\.js$/, '.min.js'),
+				format: 'es',
+			}
+		]
+	},
+	{
+		input: pkg.module,
+		plugins: [
+			babel({
+				babelrc: false,
+				presets: [['@babel/env', { targets: 'ie 6' }]]
+			}),
+			uglify(minOpts)
+		],
+		output: [
+			{
+				file: pkg.main.replace(/\.js$/, '.min.js'),
 				name: 'Base64',
 				format: 'umd'
 			}
